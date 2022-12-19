ce2c65a933e41c5c4d989e199f317e7120b3e1da
OmarBlue <omarescala17@gmail.com>
2022-12-19 16:11:30 +0000

    Added CRUD oprations

--- cinemaManager/urls.py
+++ cinemaManager/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from cinemaManager import views
 from cinemaManager.models import LogMessage
+from . import views
 
 home_list_view = views.HomeListView.as_view(
     queryset=LogMessage.objects.order_by("-log_date")[:5],  # :10 limits the results to the five most recent
@@ -11,5 +12,8 @@ home_list_view = views.HomeListView.as_view(
 urlpatterns = [
     path("", home_list_view, name="home"),
     path("addFilm/", views.addFilm, name="addFilm"),
+    path("delete/<int:id>", views.delete, name="delete"),
+    path('update/<int:id>', views.update, name='update'),
+    path('update/updatefilm/<int:id>', views.updatefilm, name='updatefilm'),
 ]
 
--- cinemaManager/views.py
+++ cinemaManager/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import redirect
 from cinemaManager.forms import LogMessageForm
 from cinemaManager.models import LogMessage
 from django.views.generic import ListView
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import LogMessage
+from django.template.loader import get_template 
 
 class HomeListView(ListView):
     """Renders the home page, with a list of all messages."""
@@ -26,3 +30,29 @@ def addFilm(request):
             return redirect("home")
     else:
         return render(request, "cinemaManager/addFilm.html", {"form": form})
+
+def delete(request,id):
+    film = LogMessage.objects.get(id=id) 
+    film.delete()
+    return HttpResponseRedirect(reverse("home"))
+
+def update(request, id):
+    films = LogMessage.objects.get(id=id)
+    template = get_template("cinemaManager/updateFilm.html")
+    context = {
+        "films": films,
+    }
+    return HttpResponse(template.render(context, request))
+
+def updatefilm(request, id):
+  title = request.POST['title']
+  age_rating = request.POST['age_rating']
+  duration = request.POST['duration']
+  description = request.POST['description']
+  film = LogMessage.objects.get(id=id)
+  film.title = title
+  film.age_rating = age_rating
+  film.duration = duration
+  film.description = description
+  film.save()
+  return HttpResponseRedirect(reverse('home'))
